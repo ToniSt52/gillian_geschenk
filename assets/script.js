f963b541090d6bada7f0eb8ce203372a69ea2a06
ToniSt52 <toni@Desktop-Toni.>
2024-12-28 20:14:34 +0100

    Voll funktionsfähig

--- assets/script.js
+++ assets/script.js
@@ -10,12 +10,15 @@ function checkCode() {
     document.body.style.backgroundRepeat = "no-repeat";
     document.body.style.backgroundAttachment = "fixed";
 
+    document.getElementById('infoText').innerText = "Glückwunsch! Du hast den richtigen Wichtel gefunden!";
+
     // Set a delay before showing the alert
     setTimeout(function() {
-      alert("Glückwunsch du hast deinen Wichtel gefunden. Beim nächsten mal wird er dir einen Al Gadafi ausgeben :D");
-    }, 10); // 10ms = 0.01 second delay (you can adjust this time)// Change the background after success
+      alert("Glückwunsch du hast deinen Wichtel gefunden. Beim nächsten mal wird er dir einen Al Gadafi spendieren :D");
+    }, 40); // 10ms = 0.01 second delay (you can adjust this time)// Change the background after success
 } else {
   // Display the failure message
-  alert("Leider Falsch! Versuche s mit einer anderen Person");
+  alert("Leider Falsch! Versuche es mit einer anderen Person");
+  document.getElementById('infoText').innerText = "Um den Rest ihres Geschenkes zu erfahren, werter Kollege, geben sie hier doch bitte das Geburtsdatum desjenigen Wichtels an, von dem sie vermuten dass dieser ihnen dieses Geschenk bereitstellte :D (DD.MM.YYYY)";
 }
 }
